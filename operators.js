b64eacceede1c6345231eb8f30dfe6d0ad97cd34
mayusha <mayushachowdary95@gmail.com>
2021-12-13 17:22:32 +0530

    hi

---
+++ operators.js
@@ -0,0 +1,34 @@
+var x=20;
+var y=30;
+var sum=x+y;
+console.log(sum);
+
+var x=40;
+var y=20;
+var diff=x-y;
+console.log(diff);
+
+var x=40;
+var y=20;
+var div=x/y;
+console.log(div);
+
+var x=40;
+var y=20;
+var mul=x*y;
+console.log(mul);
+
+var x=13;
+var y=2;
+var modulus=x%y;
+console.log(modulus);
+
+var x=8;
+ x++;        //increment operator
+var z=x;
+console.log(z);
+
+var y=40;
+ y--;
+var z=y;        //decrement operators
+console.log(z);
